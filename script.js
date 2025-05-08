b790d688e9f89081aad8ffbe86cea44a7dfda3d4
Arthur de Almeida Cecconi <arthurcecconi08@gmail.com>
2025-05-08 15:40:13 -0300

    continuidade do projeto

--- script.js
+++ script.js
@@ -11,14 +11,67 @@ function escolhertamanho() {
             }
             break;
         case '250':
-            imagemcc.src = "imagens/cocacola250ml.webp";
+            if (!iszero) {
+                imagemcc.src = "imagens/cocazero250ml.webp"
+            } else {
+                imagemcc.src = "imagens/cocacola250ml.webp";
+            }
             break;
         case '290':
-            imagemcc.src = "imagens/cocaks.jpg";
+            if (!iszero) {
+                imagemcc.src = "imagens/ks0.webp"
+            } else {
+                imagemcc.src = "imagens/cocaks.jpg";
+            }
             break;
         case '310':
-            imagemcc.src = "imagens/cocacola310ml.jfif";
+            if (!iszero) {
+                imagemcc.src = "imagens/cocazero310ml.jfif"
+            } else {
+                imagemcc.src = "imagens/cocacola310ml.jfif";
+            }
+            break;
+        case '350':
+            if (!iszero) {
+                imagemcc.src = "imagens/cocazero350ml.jpg"
+            } else {
+                imagemcc.src = "imagens/cocacola350ml.jfif";
+            }
+            break;
+        case '600':
+            if (!iszero) {
+                imagemcc.src = "imagens/cocazero600ml.png"
+            } else {
+                imagemcc.src = "imagens/cocacola600ml.webp";
+            }
+            break;
+        case '1000':
+            if (!iszero) {
+                imagemcc.src = "imagens/cocazero1l.webp"
+            } else {
+                imagemcc.src = "imagens/cocacola1l.webp";
+            }
+            break;
+        case '1500':
+            if (!iszero) {
+                imagemcc.src = "imagens/cocazero1_5l.webp"
+            } else {
+                imagemcc.src = "imagens/cocacola1_5l.jpeg";
+            }
+            break;
+        case '2000':
+            if (!iszero) {
+                imagemcc.src = "imagens/cocazero2l.png"
+            } else {
+                imagemcc.src = "imagens/cocacola2l.png";
+            }
+            break;
+        case '2500':
+            if (!iszero) {
+                imagemcc.src = "imagens/cocazero2_5l.webp"
+            } else {
+                imagemcc.src = "imagens/cocacola2_5l.webp";
+            }
             break;
     }
-    
 }
